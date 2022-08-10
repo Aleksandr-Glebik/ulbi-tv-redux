c927766ec9063ca08dce6c1b05cb60c4dab3e0de
Aleksandr-Glebik <glebikaa@mail.ru>
2022-08-11 00:14:46 +0300

    feat: add store, reducer, defaultState; App: add func addCashm getCash

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import React from "react"
+import { useDispatch, useSelector } from "react-redux"
+import cl from './App.module.css'
 
 function App() {
+
+  const dispatch = useDispatch()
+  const cash = useSelector(state => state.cash)
+  console.log('cash', cash);
+
+  const addCash = (cash) => {
+    dispatch({type: "ADD_CASH", payload: cash})
+  }
+
+  const getCash = (cash) => {
+    dispatch({type: "GET_CASH", payload: cash})
+  }
+
   return (
-    <div>
-      <h1>Start learne redux</h1>
+    <div className={cl.app}>
+      <div className={cl.app__cash}>{cash}</div>
+      <div className={cl.btn__container}>
+          <button className={cl.btn} onClick={() => addCash(Number(prompt()))}>Пополнить счет</button>
+          <button className={cl.btn} onClick={() => getCash(Number(prompt()))}>Снять со счета</button>
+      </div>
+
     </div>
   )
 }
